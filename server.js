6bbaed6740d75c198ddb6f56b94152dcee32628c
Drew Cannedy <dxc142830@utdallas.edu>
2018-10-28 20:41:54 -0500

    turns out heroku does not support tcp

--- server.js
+++ server.js
@@ -1,85 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const hbs = require('hbs');
-const queryString = require('query-string');
-
-var { mongoose } = require('./db/mongoose');
-var { Instance } = require('./models/instance');
-
-
-// https://techbrij.com/node-js-tcp-server-client-promisify
-const net = require('net');
-
-// Configuration parameters
-const HOST = process.env.IP ||'localhost';
-const PORT = process.env.PORT || 3000;
-
-// Create Server instance 
-var server = net.createServer(onClientConnected);
-
-server.listen(PORT, HOST, () => {
-  console.log('Server listening on', server.address());
-});
-
-function onClientConnected(sock) {
-  var remoteAddress = sock.remoteAddress + ':' + sock.remotePort;
-  console.log('New client connected:', remoteAddress);
-
-  sock.on('data', (data) => {
-    const parsed = queryString.parse(data);
-    if (parsed.command == "create") {
-      var instance = new Instance();
-
-      instance.save().then((doc) => {
-        sock.write({id: doc.id});
-      }, (err) => {
-        sock.write({error: err});
-      });
-    } else if (parsed.command == "delete") {
-      id = parsed.id;
-      Instance.findByIdAndRemove(id, (err, doc) => {
-        if (err) {
-          return sock.write({error: err});
-        }
-        sock.write({result: "success"});
-      });
-    } else if (parsed.command == "update") {
-      var id = parsed.id;
-      Instance.findById(id).then((instance) => {
-        if (parsed.o_spartanX) {
-          instance.o_spartanX =parsed.o_spartanX;
-        }
-        if (parsed.o_spartanY) {
-          instance.o_spartanY = parsed.o_spartanY;
-        }
-        if (parsed.o_minotaurX) {
-          instance.o_minotaurX = parsed.o_minotaurX;
-        }
-        if (parsed.o_minotaurY) {
-          instance.o_minotaurY = parsed.o_minotaurY;
-        }
-        instance.save();
-        sock.write({result: "success"});
-      }, (err) => {
-        sock.write({error: err});
-      });
-    }
-    console.log('%s says: %s', remoteAddress, data);
-  });
-
-  sock.on('close', function () {
-    console.log('connection from %s closed', remoteAddress);
-  });
-
-  sock.on('error', function (err) {
-    console.log('Connection %s error: %s', remoteAddress, err.message);
-  });
-};
-
 
 var app = express();
 app.use(bodyParser.json());
-
 
 app.get('/instance', (req, res) => {
   if (req.query.id) {
