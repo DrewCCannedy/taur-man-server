1e649eaeaf4c41f2659edb46396273e2b75b6d06
Drew Cannedy <dxc142830@utdallas.edu>
2018-10-29 13:26:32 -0500

    re-enabling post requests

--- server.js
+++ server.js
@@ -41,49 +41,49 @@ app.listen(PORT, () => {
   console.log(`Started express server on port ${PORT}`);
 });
 
-// app.post('/create', (req, res) => {
-//   var instance = new Instance({
-//     humanX: 0,
-//     humanY: 0,
-//     taurX: 0,
-//     taurY: 0
-//   });
+app.post('/create', (req, res) => {
+  var instance = new Instance({
+    humanX: 0,
+    humanY: 0,
+    taurX: 0,
+    taurY: 0
+  });
 
-//   instance.save().then((doc) => {
-//     res.status(200).json({ id: doc.id });
-//   }, (err) => {
-//     res.status(400).send(err);
-//   });
-// });
+  instance.save().then((doc) => {
+    res.status(200).json({ id: doc.id });
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
 
-// app.post('/delete', (req, res) => {
-//   delete_id = req.query.id;
-//   Instance.findByIdAndRemove(delete_id, (err, doc) => {
-//     if (err) {
-//       return res.send(err);
-//     }
-//     return res.status(200).send();
-//   })
-// })
+app.post('/delete', (req, res) => {
+  delete_id = req.query.id;
+  Instance.findByIdAndRemove(delete_id, (err, doc) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.status(200).send();
+  })
+})
 
-// app.post('/instance', (req, res) => {
-//   id = req.query.id;
-//   Instance.findById(id).then((instance) => {
-//     if (req.query.o_spartanX) {
-//       instance.o_spartanX = req.query.o_spartanX;
-//     }
-//     if (req.query.o_spartanY) {
-//       instance.o_spartanY = req.query.o_spartanY;
-//     }
-//     if (req.query.o_minotaurX) {
-//       instance.o_minotaurX = req.query.o_minotaurX;
-//     }
-//     if (req.query.o_minotaurY) {
-//       instance.o_minotaurY = req.query.o_minotaurY;
-//     }
-//     instance.save();
-//     res.send();
-//   }, (e) => {
-//     res.status(400).send(e);
-//   });
-// });
+app.post('/instance', (req, res) => {
+  id = req.query.id;
+  Instance.findById(id).then((instance) => {
+    if (req.query.o_spartanX) {
+      instance.o_spartanX = req.query.o_spartanX;
+    }
+    if (req.query.o_spartanY) {
+      instance.o_spartanY = req.query.o_spartanY;
+    }
+    if (req.query.o_minotaurX) {
+      instance.o_minotaurX = req.query.o_minotaurX;
+    }
+    if (req.query.o_minotaurY) {
+      instance.o_minotaurY = req.query.o_minotaurY;
+    }
+    instance.save();
+    res.send();
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
